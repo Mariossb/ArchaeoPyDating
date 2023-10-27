e0a1d551f35a4d5fdf80b5873416d17a245979a0
Mario Serrano <145672592+Mariossb@users.noreply.github.com>
2023-10-27 17:31:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ArchaeoPyDating',  
+    version='0.1',  
+    packages=find_packages(),  
+    install_requires=open('requirements.txt').read().splitlines(),  
+    description='Python tool for archaeomagnetic dating',
+    author='Mario Serrano',  
+    author_email='marioser@ucm.es',  
+    url='https://github.com/Mariossb/ArchaeoPyDating',  
+)
